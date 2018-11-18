0aa83225e79b63a9102009acdd45cecac2fedd3a
John Kussmann <jkussmann@outlook.com>
2018-11-17 22:26:28 -0800

    change process handling for CoreNLP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import socket
+import time
 import pos_tagger as pt
 
 from subprocess import Popen, CREATE_NEW_CONSOLE
@@ -7,8 +9,12 @@ from sys import executable
 def get_input():
 	line = ''
 
-	#Start the Stanford CoreNLP server
-	proc1 = Popen([executable, 'core_nlp.py'], cwd='c:\stanford-corenlp-full-2018-02-27', creationflags=CREATE_NEW_CONSOLE)
+	#Start the Stanford CoreNLP server if it isn't running
+	result = coreNLP_status()
+	if result != 0:
+		proc1 = Popen([executable, 'core_nlp.py'], cwd='c:\stanford-corenlp-full-2018-02-27', creationflags=CREATE_NEW_CONSOLE)
+		print ('Initializing CoreNLP....')  #Give CoreNLP some time to get going before accepting input.
+		time.sleep(30)
 
 	try:
 		while (line != 'end'):
@@ -37,6 +43,12 @@ def get_input():
 	except KeyboardInterrupt:
 		sys.exit()
 
+def coreNLP_status():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	result = sock.connect_ex(('localhost', 9000))
+
+	return result
+	
 def main():
 	"""
 	This is the main entry point for the program
